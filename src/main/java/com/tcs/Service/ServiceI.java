3fbbd3aeb7624d409042127da4df20b2699649ea
vishalmatale <you@example.com>
2022-08-07 19:42:05 +0530

    ServiceI layer

---
+++ src/main/java/com/tcs/Service/ServiceI.java
@@ -0,0 +1,16 @@
+package com.tcs.Service;
+
+public class ServiceI {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hellow I am vishal matale");
+		
+		System.out.println("Hellow I am ganesh matale");
+		
+		
+	}
+	
+
+}
