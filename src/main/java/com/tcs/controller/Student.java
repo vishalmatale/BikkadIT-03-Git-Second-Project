a629efd34a7ec9e1217fdd472b8a6ac5ff6a5480
vishalmatale <you@example.com>
2022-08-07 17:41:15 +0530

    controller layer

---
+++ src/main/java/com/tcs/controller/Student.java
@@ -0,0 +1,18 @@
+package com.tcs.controller;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+		
+	System.out.println("Hellow good mornining all of you");	
+		
+	
+	
+	
+	
+	}
+	
+	
+
+}
