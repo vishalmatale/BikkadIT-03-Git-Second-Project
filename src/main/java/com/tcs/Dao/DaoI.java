b3c0362624966b3ace6a0b074cc06c633fa4c9f6
vishalmatale <you@example.com>
2022-08-07 19:38:17 +0530

    Dao layer

---
+++ src/main/java/com/tcs/Dao/DaoI.java
@@ -0,0 +1,15 @@
+package com.tcs.Dao;
+
+public class DaoI {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Daoi layer method");
+		System.out.println("Daoi layer class");
+		
+		
+		
+	}
+	
+
+}
